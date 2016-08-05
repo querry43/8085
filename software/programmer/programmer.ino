0a9d4ca749a41fcd6c3a072f2ffa1726d20c4ac4
Matt Harrington <querry43@underdogma.net>
2016-08-04 20:40:04 -0700

    successful test of output program

--- software/programmer/programmer.ino
+++ software/programmer/programmer.ino
@@ -2,6 +2,7 @@
 #include "noop.h"
 #include "light_on.h"
 #include "blink.h"
+#include "output.h"
 
 const uint8_t
   HOLD_pin = 8,
@@ -16,7 +17,7 @@ const uint8_t
  *  A8-A15 to PORTA (22-29)
  *  D0-D7 to PORTL (49-42)
  */
-#define PROGRAM blink_asm
+#define PROGRAM output_asm
 #define MEM_SIZE 4096
 #define DEBUG false
 #define MEMTEST false
