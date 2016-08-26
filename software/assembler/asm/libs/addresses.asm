a95355394932e2f8a32d076af28a4bce9a12254f
Matt Harrington <querry43@gmail.com>
2016-08-25 22:19:42 -0700

    trying to figure out the 8279

--- software/assembler/asm/libs/addresses.asm
+++ software/assembler/asm/libs/addresses.asm
@@ -1,5 +1,10 @@
-; output CS is 1001_1000_0000_0000
+; using the 8155-2 chip
+; MSB is IO/!M
 
 CSADDY SET 9800H
 PAADDY SET 9801H
 PBADDY SET 9802H
+
+; using the 8279 chip
+; MSB is A0
+; start address is 0010_0000_0000_0000
