dcb06ef46bc5718ec84a0eda380ba9ed32de1adc
Matt Harrington <querry43@gmail.com>
2017-10-15 20:01:28 -0700

    moving hardware interface back into place

---
+++ software/programmer/hardware_interface.cpp
@@ -0,0 +1,93 @@
+#include <arduino.h>
+#include "hardware_interface.h"
+
+#ifdef MOCK_HARDWARE
+
+void hold_and_commandeer_bus() { }
+void release_bus() { }
+void release_hold() { }
+void reset_cpu() { }
+
+void write_mem(const uint16_t addr, const uint8_t data) { }
+uint8_t read_mem(const uint16_t addr) { }
+
+#else
+
+void set_address(const uint16_t addr) {
+  DDRC = 0xff;
+  DDRA = 0xff;
+
+  PORTC = addr;
+  PORTA = addr >> 8;
+}
+
+void set_data(const uint8_t data) {
+  DDRL = 0xff;
+  PORTL = data;
+}
+
+void write_mem(const uint16_t addr, const uint8_t data) {
+  set_address(addr);
+
+  digitalWrite(WR_pin, LOW);
+  set_data(data);
+  digitalWrite(WR_pin, HIGH);
+
+  DDRC = 0;
+  DDRA = 0;
+  DDRL = 0;
+}
+
+uint8_t read_mem(const uint16_t addr) {
+  set_address(addr);
+
+  digitalWrite(RD_pin, LOW);
+  uint8_t data = PINL;
+  digitalWrite(RD_pin, HIGH);
+
+  DDRC = 0;
+  DDRA = 0;
+  DDRL = 0;
+
+  return data;
+}
+
+void hold_and_commandeer_bus() {
+  digitalWrite(HOLD_pin, HIGH);
+  pinMode(HOLD_pin, OUTPUT);
+  delay(100);
+
+  pinMode(LATCH_EN_pin, OUTPUT);
+  pinMode(RD_pin, OUTPUT);
+  pinMode(WR_pin, OUTPUT);
+
+  digitalWrite(LATCH_EN_pin, HIGH);
+  digitalWrite(RD_pin, HIGH);
+  digitalWrite(WR_pin, HIGH);
+
+  digitalWrite(RESET_pin, HIGH);
+  pinMode(RESET_pin, OUTPUT);
+}
+
+void release_bus() {
+  pinMode(LATCH_EN_pin, INPUT);
+  pinMode(RD_pin, INPUT);
+  pinMode(WR_pin, INPUT);
+
+  PORTC = 0;
+  PORTA = 0;
+  PORTL = 0;
+}
+
+void release_hold() {
+  digitalWrite(HOLD_pin, LOW);
+  pinMode(HOLD_pin, INPUT);
+}
+
+void reset_cpu() {
+  digitalWrite(RESET_pin, LOW);
+  delay(100);
+  pinMode(RESET_pin, INPUT); // high impedance lets reset circuit take over
+}
+
+#endif
---
+++ software/programmer/hardware_interface.h
@@ -0,0 +1,27 @@
+#pragma once
+
+//#define MOCK_HARDWARE
+
+
+/* Hookup:
+ *  A0-A7 to PORTC (37-30)
+ *  A8-A15 to PORTA (22-29)
+ *  D0-D7 to PORTL (49-42)
+ */
+
+#include <stdint.h>
+
+const uint8_t
+  HOLD_pin = 8,
+  RESET_pin = 9,
+  RD_pin = 10,
+  WR_pin = 11,
+  LATCH_EN_pin = 12;
+
+void hold_and_commandeer_bus();
+void release_bus();
+void release_hold();
+void reset_cpu();
+
+void write_mem(const uint16_t addr, const uint8_t data);
+uint8_t read_mem(const uint16_t addr);
