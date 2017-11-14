7b8e6cbcc9116dc3a61542f9e579189b6c34c624
Matt Harrington <querry43@gmail.com>
2017-11-13 21:55:53 -0800

    organizing the programmer into a class

--- software/programmer/programmer.py
+++ software/programmer/programmer.py
@@ -2,80 +2,127 @@
 
 import sys, getopt, serial, time, os
 
-def execute_command(serial_obj, command_str):
-    print("\nexecuting command: " + command_str)
-    serial_obj.write(command_str + '\n')
-    got_ok = False
-    while True:
-        line = serial_obj.readline().rstrip()
-        if (line[:2] == "OK"):
-            got_ok = True
-        if (not line):
-            break
-        print(line)
-    if (not got_ok):
-        raise Exception('Command ' + command_str + ' failed')
+class Programmer:
+    def __init__(self, port, baudrate, timeout):
+        self.timeout = timeout
+        self.__open_serial(port, baudrate, self.timeout)
+
+    def toggle_debug(self):
+        self.__execute_command('d');
+
+    def set_rom_size(self, size):
+        self.__execute_command('m|' + str(size))
+
+    def set_rom_offset(self, offset):
+        self.__execute_command('o|' + str(offset))
+
+    def zero_rom(self):
+        self.__execute_command('z')
+
+    def read_rom(self):
+        self.__execute_command('r')
+
+    def write_rom(self, hex_line):
+        self.__execute_command('w|' + hex_line)
+
+    def hold_bus(self):
+        self.__execute_command('h')
+
+    def release_bus(self):
+        self.__execute_command('l')
+
+    def __open_serial(self, port, baudrate, timeout):
+        self.serial_obj = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
+        print("Connecting to device: " + self.serial_obj.name + " (" + str(baudrate) + ")")
+        time.sleep(timeout)
+        self.__execute_command('p')
+
+    def __execute_command(self, command_str):
+        print("executing command: " + command_str)
+        self.serial_obj.write(command_str + '\n')
+        got_ok = False
+        while True:
+            line = self.serial_obj.readline().rstrip()
+            print(line)
+            if (line[:2] == "OK"):
+                got_ok = True
+                break
+            if (not line):
+                break
+        if (not got_ok):
+            raise Exception('Command ' + command_str + ' failed')
 
 def usage():
-    print("USAGE: " + os.path.basename(__file__) + " --port /dev/cu.usbmodem1411 --rom-size 1024 [--rom-offset 0] [--baud 38400] [--zero] [--read] [file1.hex ...]")
+    print("USAGE: " + os.path.basename(__file__) + " --port /dev/cu.usbmodem1411 [--rom-size 2048] [--rom-offset 0] [--baud 38400] [--zero] [--read] [file1.hex ...]")
     sys.exit()
 
-opts, args = getopt.getopt(sys.argv[1:], '', ['help','h','?','zero','debug','rom-size=','rom-offset=','port=','baud=', 'read'])
+def main():
+    opts, args = getopt.getopt(
+        sys.argv[1:],
+        '',
+        ['help','h','?','zero','debug','rom-size=','rom-offset=','port=','baud=', 'read']
+    )
 
-baud_rate = 38400
-do_debug = False
-do_mem_clear = False
-do_read = False
-rom_offset = "0"
-rom_size = "2048"
-serial_port = ""
-timeout = 1
+    baudrate = 38400
+    port = None
+    timeout = 1
 
-for o, v in opts:
-    if o == "--zero":
-        do_mem_clear = True
-    elif o == "--rom-size":
-        rom_size = v
-    elif o == "--rom-offset":
-        rom_offset = v
-    elif o == "--debug":
-        do_debug = True
-    elif o == "--port":
-        serial_port = v
-    elif o == "--baud":
-        baud_rate = v
-    elif o == "--read":
-        do_read = True
-    elif o in ("--help", "-h", "-?"):
+    rom_offset = 0
+    rom_size = 2048
+
+    do_debug = False
+    do_zero_rom = False
+    do_read = False
+
+    for o, v in opts:
+        if o == "--port":
+            port = v
+        elif o == "--baud":
+            baudrate = int(v)
+
+        elif o == "--rom-size":
+            rom_size = int(v)
+        elif o == "--rom-offset":
+            rom_offset = int(v)
+
+        elif o == "--debug":
+            do_debug = True
+        elif o == "--zero":
+            do_zero_rom = True
+        elif o == "--read":
+            do_read = True
+        elif o in ("--help", "-h", "-?"):
+            usage()
+
+    if port is None:
         usage()
 
-line = ""
+    programmer = Programmer(
+        port = port,
+        baudrate = baudrate,
+        timeout = timeout
+    )
 
-arduino_serial = serial.Serial(serial_port, baudrate=baud_rate, timeout=timeout)  # open serial port
-print("Connecting to device: " + arduino_serial.name)
-time.sleep(timeout)
+    if(do_debug):
+        programmer.toggle_debug()
 
-if(do_debug):
-    execute_command(arduino_serial, 'd')
+    programmer.set_rom_size(rom_size)
+    programmer.set_rom_offset(rom_offset)
+    programmer.hold_bus()
 
-execute_command(arduino_serial, 'm|' + rom_size)
-execute_command(arduino_serial, 'o|' + rom_offset)
+    if(do_zero_rom):
+        programmer.zero_rom()
 
-execute_command(arduino_serial, 'h')
-
-if(do_mem_clear):
-    execute_command(arduino_serial, 'z')
-
-if(len(args) > 0):
     for f_path in args:
-        with open(f_path) as f:
-            f_lines = f.readlines()
-        for line in f_lines:
-            execute_command(arduino_serial, 'w|' + line)
+        with open(f_path, 'r') as f:
+            for line in f:
+                programmer.write_rom(line)
 
-if(do_read):
-    execute_command(arduino_serial, 'r')
+    if(do_read):
+        programmer.read_rom()
 
-execute_command(arduino_serial, 'l')
+    programmer.release_bus()
 
-arduino_serial.close()
+if __name__ == "__main__":
+    main()
+
