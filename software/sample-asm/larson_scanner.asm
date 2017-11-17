15851d4cbee8f1b7323dda7291ce47458089e2fa
Matt Harrington <matt.harrington@rentrak.com>
2017-11-16 16:05:02 -0800

    organizing asm files

--- software/sample-asm/larson_scanner.asm
+++ software/sample-asm/larson_scanner.asm
@@ -1,28 +1,59 @@
+; a larson scanner repeating a stored pattern
 ; for the 8155
 
         LXI SP , 8191
         JMP START
 
         ORG 44H
-START:  LXI H,CSADDY
-        MVI M,00000001B ; PA output
+START:
+        ; configure ports
+        LXI H,CSADDY
+        MVI M,00000011B
 
-        MVI A,1
+
+BEGIN:  LXI D,PTN
+
+LOOP:   LDAX D          ; write first byte to B port and second byte to A port
+        LXI H,PBADDY
+        MOV M,A
+        INX D
+
+        LDAX D
         LXI H,PAADDY
+        MOV M,A
+        INX D
 
-        ; set delay timer
+        CALL LDELAY
+
+        MOV A,E         ; start over if at end of a-pattern
+        CPI PTNEND
+        JZ BEGIN
+        JMP LOOP
+
+          ; port-B    port-A
+PTN:    DB 00000000B,00000001B
+        DB 00000000B,00000010B
+        DB 00000000B,00000100B
+        DB 00000000B,00001000B
+        DB 00000000B,00010000B
+        DB 00000000B,00100000B
+        DB 00000000B,01000000B
+        DB 00000000B,10000000B
+        DB 00000001B,00000000B
+        DB 00000010B,00000000B
+        DB 00000001B,00000000B
+        DB 00000000B,10000000B
+        DB 00000000B,01000000B
+        DB 00000000B,00100000B
+        DB 00000000B,00010000B
+        DB 00000000B,00001000B
+        DB 00000000B,00000100B
+        DB 00000000B,00000010B
+
+PTNEND: NOP
+
+LDELAY: PUSH B
         LXI B,2000H
-
-FWD:    MOV M,A
-        RLC
         CALL DELAY
-        CPI 80H
-        JNZ FWD
-
-REV:    MOV M,A
-        RRC
-        CALL DELAY
-        CPI 1H
-        JNZ REV
-
-        JMP FWD
+        POP B
+        RET
